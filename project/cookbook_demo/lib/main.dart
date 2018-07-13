b9e538cb6b6d4277264597196ae48aec805f767b
HeathWang <yishu.jay@gmail.com>
2018-07-13 17:11:18 +0800

    implement cookbook main page.

--- project/cookbook_demo/lib/main.dart
+++ project/cookbook_demo/lib/main.dart
@@ -1,3 +1,50 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MainPage());
+}
 
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CookBook',
+      initialRoute: '/',
+      routes: <String, WidgetBuilder> {
+        '/': (BuildContext context) => MyMenuPage(),
+      },
+      theme: ThemeData(primaryColor: Colors.blue),
+    );
+  }
+}
+
+class MyMenuPage extends StatelessWidget {
+
+  final menus = [
+    'Displaying SnackBars',
+    'Working with Tabs',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CookBook'),),
+      body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(menus[index]),
+                  onTap: (){
+
+                  },
+                ),
+                Divider(height: 0.5,)
+              ],
+            );
+          },
+        itemCount: menus.length,
+      ),
+    );
+  }
+}
