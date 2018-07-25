b80154ad5d891bec457d8ddfe7a66b9a083814c4
HeathWang <yishu.jay@gmail.com>
2018-07-25 17:57:09 +0800

    整理目录

--- project/advance_demo/lib/main.dart
+++ project/advance_demo/lib/main.dart
@@ -1,113 +1,51 @@
 import 'package:flutter/material.dart';
+import 'sliver_page.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyHomePage(),
-    ));
+void main() => runApp(MyHomeList());
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageState();
-  }
-}
 
-class MyHomePageState extends State<MyHomePage> {
+class MyHomeList extends StatelessWidget {
 
-  ScrollController _scrollController;
-
-  @override
-  void initState() {
-    _scrollController = ScrollController();
-    _scrollController.addListener(() {
-      setState(() {
-
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
+  final demos = ['DYNAMIC SLIVER FLOATINGACTIONBUTTON'];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          CustomScrollView(
-            controller: _scrollController,
-            slivers: <Widget>[
-              SliverAppBar(
-                expandedHeight: 256.0,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text('SliverFab Example'),
-                  background: Stack(
-                    fit: StackFit.expand,
-                    children: <Widget>[
-                      Image.asset('images/conver.jpg', fit: BoxFit.cover,),
-                      DecoratedBox(decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: const <Color>[const Color(0x60000000), const Color(0x00000000)],
-                          begin: Alignment(0.0, -1.0),
-                          end: Alignment(0.0, -0.4),
-                        ),
-                      )),
-                    ],
+    return MaterialApp(
+      routes: <String, WidgetBuilder> {
+        '/sliver': (_) => MyHomePage(),
+
+      },
+      home: Scaffold(
+        appBar: AppBar(title: Text('Home'),),
+        body: ListView.builder(
+            itemBuilder: (context, i) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ListTile(title: Text(demos[i]), onTap: (){
+                    tapCell(i, context);
+                  },),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Divider(),
                   ),
-                ),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  List.generate(20, (int index) {
-                    return ListTile(
-                      title: Text('Item $index'),
-                    );
-                  }),
-                ),
-              ),
-            ],
-          ),
-          _buildFb(),
-        ],
+                ],
+              );
+            },
+          itemCount: demos.length,
+        ),
       ),
     );
   }
 
-  Widget _buildFb() {
-    final double defaultTopMargin = 256.0 - 4.0;
-    //pixels from top where scaling should start
-    final double scaleStart = 96.0;
-    //pixels from top where scaling should end
-    final double scaleEnd = scaleStart / 2;
-
-    double top = defaultTopMargin;
-    double scale = 1.0;
-
-    if (_scrollController.hasClients) {
-      double offset = _scrollController.offset;
-      top -= offset;
-
-      if (offset < defaultTopMargin - scaleStart) {
-        scale = 1.0;
-      } else if (offset < defaultTopMargin - scaleEnd) {
-        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
-      } else {
-        scale = 0.0;
-      }
+  void tapCell(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.of(context).pushNamed('/sliver');
+        break;
     }
-
-    return Positioned(
-      top: top,
-      right: 16.0,
-      child: Transform(
-        transform: Matrix4.identity()..scale(scale),
-        alignment: Alignment.center,
-        child: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),),
-      ),
-    );
   }
+
 }
+
